5bd72624efe12d73dc0d62546ca8cb1a16b25e87
Juana Diaz <estidy@gmail.com>
2025-05-13 09:39:12 -0300

    creacion del proyecto y agregando tailwind

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Form from "./components/form"
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de Calorias
+          </h1>
+        </div>
+      </header>
+      <section>
+        <div>
+          <Form />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
