e41ac2efe92d46a4dd29f449b52a7bf392592c8d
Juana Diaz <estidy@gmail.com>
2025-05-13 11:10:33 -0300

    formulario para agregar categorias, actividad y calorias

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,62 @@
+import { categories } from "../data/categories"
+
 export default function Form() {
   return (
-    <div>
-      <h1>form</h1>
-    </div>
+      <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <div className="grid grid-cols-1 gap-3">
+          <label 
+            htmlFor="category" 
+            className="font-bold"
+          >
+            Categoria: 
+          </label>
+          <select 
+            className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category">
+              {categories.map(category =>(
+                  <option 
+                  key= {category.id}
+                  value= {category.id}
+                  >
+                    {category.name}
+                  </option>
+              ))
+
+              }
+          </select>
+        </div>
+        <div className="grid grid-cols-1 gap-3">
+            <label 
+              htmlFor="activity"
+              className="font-bold"
+            >
+              Actividad: 
+            </label>
+            <input
+               type="text" 
+               id="activity"
+               className="border border-slate-300 p-2 rounded-lg"
+               placeholder="Ej: Comida,Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+              />
+        </div>
+        <div className="grid grid-cols-1 gap-3">
+            <label 
+              htmlFor="calories"
+              className="font-bold"
+            >
+              Calorias: 
+            </label>
+            <input 
+              type="number" 
+              id="calories"
+              className="border border-slate-300 p-2 rounded-lg"
+              placeholder="Calorias Ej: 300 o 500"
+            />
+        </div>
+        <input 
+          type="submit"
+          className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+          value="Guardar" 
+        />
+      </form>
   )
 }
